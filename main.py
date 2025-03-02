3a966757ae060e856b331304bfad063be9cec979
Beastman-Dev <wmeastman@gmail.com>
2025-03-02 13:25:48 -0500

    Added pack order and padding parameters

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+import tkinter as tk
 
 class MyApp:
 
@@ -7,15 +7,15 @@ class MyApp:
 
         # Configuring parameters of the window
         root.title("My First Tkinter App")
-        root.maxsize(800, 600)
-        root.geometry("640x480")
+        # root.maxsize(800, 600)
+        # root.geometry("640x480")
 
         # Creating widgets - in this case text widgets
         # First we define the widgets
         self.label_text = StringVar()
         label = Label(root, text="Some label text", textvariable=self.label_text)
         # Then we draw the widgets to the window
-        label.pack()
+        label.pack(side=tk.LEFT)
 
         # Modifying existing widgets can be done one attribute at a time with the dictionary method        
         # label["text"] = "New text"
@@ -30,7 +30,7 @@ class MyApp:
         # Create the widget instance from the "Entry" class, bound to a different variable (entry)
         entry = Entry(root, textvariable=self.entry_text)
         # Draw the widget to the window
-        entry.pack()
+        entry.pack(side=tk.LEFT)
 
         # Use the dictionary method of updating widget properties to bind the entry value to the widget
         # label["textvariable"] = entry_text
@@ -38,12 +38,12 @@ class MyApp:
         #entry_text.set("Set text manually")
 
         button = Button(root, text="Button text", command=self.press_button)
-        button.pack()
+        button.pack(side=tk.LEFT)
 
         self.list_item_strings = ["Hey", "Hi", "Hello", "Howdy", "Greetings"]
         list_items = StringVar(value=self.list_item_strings)
         listbox = Listbox(root, listvariable=list_items)
-        listbox.pack()
+        listbox.pack(side=tk.LEFT, padx=20, pady=20)
         listbox["height"] = 5
         listbox.bind("<<ListboxSelect>>", lambda s: self.select_item(listbox.curselection()))
         # This is some arbitrary stuff, so just have to remember or use reference materials
